19ec6d3099d51b49a15eb11b41b7c58fc1e16dce
Gabriel Florit <gabrielflorit@gmail.com>
2013-03-05 16:37:32 -0500

    resizing pngs

--- web.js
+++ web.js
@@ -20,24 +20,17 @@ app.get('/:gist', function(req, res) {
 
 	var gist = req.params.gist;
 
-	console.log('getting webshot');
+	webshot('livecoding.io/s/' + gist, gist + '.png', options, function(err) {
+		var buf = fs.readFileSync(gist + '.png');
 
-	webshot('google.com', 'google.png', options, function(err) {
-//	webshot('livecoding.io/s/' + gist, gist + '.png', options, function(err) {
-		console.log('got webshot');
-
-		var buf = fs.readFileSync('google.png');	
-
-		imageMagick(buf, 'google.png')
-			.resize(100, 100)
-			.write('google1.png', function(err) {
-				console.log(err);
-					var img = fs.readFileSync('google1.png');
-
+		imageMagick(buf, gist + '.png')
+			.resize(320, 240)
+			.write(gist + '1.png', function(err) {
+					var img = fs.readFileSync(gist + '1.png');
 					res.writeHead(200, {'Content-Type': 'image/png' });
 					res.end(img, 'binary');
-					fs.unlink('google1.png');
-					fs.unlink('google.png');
+					fs.unlink(gist + '1.png');
+					fs.unlink(gist + '.png');
 			});
 
 	});
@@ -51,10 +44,15 @@ app.get('/:gist/:version', function(req, res) {
 
 	webshot('livecoding.io/s/' + gist + '/' + version, gist + '-' + version + '.png', options, function(err) {
 
-		var img = fs.readFileSync(gist + '-' + version + '.png');
-		res.writeHead(200, {'Content-Type': 'image/png' });
-		res.end(img, 'binary');
-		fs.unlink(gist + '-' + version + '.png');
+		imageMagick(buf, gist + '-' + version + '.png')
+			.resize(320, 240)
+			.write(gist + '-' + version + '1.png', function(err) {
+					var img = fs.readFileSync(gist + '-' + version + '1.png');
+					res.writeHead(200, {'Content-Type': 'image/png' });
+					res.end(img, 'binary');
+					fs.unlink(gist + '-' + version + '1.png');
+					fs.unlink(gist + '-' + version + '.png');
+			});
 
 	});
 
