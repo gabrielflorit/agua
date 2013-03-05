c7f6a5f9f502c06e757d2d3a0034d64aa3eacd35
Gabriel Florit <gabrielflorit@gmail.com>
2013-03-04 19:25:45 -0500

    testing

--- web.js
+++ web.js
@@ -8,12 +8,12 @@ http.createServer(function(req, res) {
 	var request = url.parse(req.url, true);
 	var action = request.pathname;
 
-	webshot('google.com', 'google.png', function(err) {
+	webshot('google.com', '/tmp/google.png', function(err) {
 
-		var img = fs.readFileSync('google.png');
+		var img = fs.readFileSync('/tmp/google.png');
 		res.writeHead(200, {'Content-Type': 'image/gif' });
 		res.end(img, 'binary');
-		fs.unlink('google.png');
+		fs.unlink('/tmp/google.png');
 
 	});
 
