194771d2cdbb955ec505d9cbc69863800834b634
Gabriel Florit <gabrielflorit@gmail.com>
2013-03-04 19:14:05 -0500

    added node skeleton

---
+++ web.js
@@ -0,0 +1,38 @@
+var fs = require('fs');
+var http = require('http');
+var url = require('url');
+var webshot = require('webshot');
+
+
+http.createServer(function(req, res) {
+	var request = url.parse(req.url, true);
+	var action = request.pathname;
+
+	webshot('google.com', 'google.png', function(err) {
+
+		var img = fs.readFileSync('./google.png');
+		res.writeHead(200, {'Content-Type': 'image/gif' });
+		res.end(img, 'binary');
+
+	});
+
+
+}).listen(process.env.PORT);
+
+
+// var express = require('express');
+// var fs = require('fs');
+
+// var app = express.createServer(express.logger());
+
+// app.get('/', function(request, response) {
+// 	response.send('Hello World!');
+// });
+
+// var port = process.env.PORT || 5000;
+// app.listen(port, function() {
+// 	console.log("Listening on " + port);
+// });
+
+
+
